50a2f5b0dc1d720591adf1d46d34e5d0a1b91b29
krish2487 <narumanchisrikrishnachaitanya@gmail.com>
2019-12-28 18:54:13 +0100

    Modified crc and checksum examples

--- cksum.c
+++ cksum.c
@@ -7,9 +7,11 @@
 #include <stdlib.h>
 
 char buf1[10]={0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF, 0x00, 0x11, 0x22, 0x55};
-
+char *buff2 = "$OWMWV,0.0,R,0.03,M,A";
 unsigned checksum(void *buffer, size_t len, unsigned int seed)
 {
+      //The following step creates a pointer and points to the array in question.
+      //Without the pointer, the arithmetic and increment for checksum wont work.
       unsigned char *buf = (unsigned char *)buffer;
       size_t i;
 
@@ -26,7 +28,7 @@ main()
 {
       FILE *fp;
       size_t len;
-      char buf[4096], *file = "checksum.c";
+      char buf[4096], *file = "cksum.c";
 
       if (NULL == (fp = fopen(file, "rb")))
       {
@@ -41,10 +43,16 @@ main()
       printf("size of  buf : %d \n", len);
       printf("The checksum of buf is %#4x\n", checksum(buf1, len, 0));
 
+      len = strlen(buff2);
+      printf("size of  buff2 : %d \n", len);
+      printf("The checksum of buff2 is %d\n", checksum(buff2, len, 0));
+
+
       uint16_t chksum =0;
+      unsigned char *tmpptr = (unsigned char*)buf1;
       for(int count =0; count < 10;count++)
       {
-            chksum += buf1[count];
+            chksum += (uint16_t)(*tmpptr++);
       }
       printf("The manual checksum of buf is %#4x\n",chksum);
 
--- crc_pololu.c
+++ crc_pololu.c
@@ -9,11 +9,12 @@ XOR the existing CRC with the present message byte,
 Loop for 8 times, for each bit in the byte -
   If the lowest bit is a '1'
   then XOR the CRC with the polynomial (which is 1001 0001 or 0x91)
+  Then shift the CRC to the right by 1 bit.
 
 
 */
 
-const unsigned char CRC7_POLY = 0x91;
+uint16_t CRC7_POLY = 0xa001;
  
 unsigned char getCRC(unsigned char message[], unsigned char length)
 {
@@ -21,7 +22,7 @@ unsigned char getCRC(unsigned char message[], unsigned char length)
  
   for (i = 0; i < length; i++)
   {
-    printf("Outer loop -  Byte # %d : %#4x, crc : %#4x\n", i, message[i], crc);
+    // printf("Outer loop -  Byte # %d : %#4x, crc : %#4x\n", i, message[i], crc);
 
     crc ^= message[i];
     for (j = 0; j < 8; j++)
@@ -29,9 +30,9 @@ unsigned char getCRC(unsigned char message[], unsigned char length)
       if (crc & 1)
         crc ^= CRC7_POLY;
       crc >>= 1;
-      printf("Inner loop -  crc : %#4x\n", crc);
+      // printf("Inner loop -  crc : %#4x\n", crc);
     }
-    printf("Inner loop exit - crc : %#4x\n", crc);
+    // printf("Inner loop exit - crc : %#4x\n", crc);
 
   }
   return crc;
@@ -41,14 +42,18 @@ int main()
 {
   // create a message array that has one extra byte to hold the CRC:
   unsigned char message[7] = {0x83, 0xAA, 0x12, 0xC3, 0x92};
-  message[5] = getCRC(message, 5);
-  printf("CRC is %#4x\n", message[5]);
-  message[6] = getCRC(message, 6);
-  printf("CRC verification check is %#4x\n", message[6]);
-  // send this message to the Simple Motor Controller
-  for (int i = 0; i < 7; ++i)
-  {
-    /* code */
-    printf("message byte #%d : %#4x\n", i,message[i]);
-  }
+  char *buff2 = "$OWMWV,0.0,R,0.03,M,A";
+
+  // message[5] = getCRC(message, 5);
+  // printf("CRC is %#4x\n", message[5]);
+  // message[6] = getCRC(message, 6);
+  // printf("CRC verification check is %#4x\n", message[6]);
+  // // send this message to the Simple Motor Controller
+
+  printf("CRC for buffer 2 is %d\n", getCRC(buff2,21));
+  // for (int i = 0; i < 7; ++i)
+  // {
+  //   /* code */
+  //   printf("message byte #%d : %#4x\n", i,message[i]);
+  // }
 }
